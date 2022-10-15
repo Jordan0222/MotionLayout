98cb63cc4876dcdf88a7ad6dc0245f9c6e2caf34
zhangyixun <asxz0121@gmail.com>
2022-10-15 21:44:47 +0800

    MotionLayout progress

--- app/src/main/java/com/jordan/motionlayout/MainActivity.kt
+++ app/src/main/java/com/jordan/motionlayout/MainActivity.kt
@@ -2,10 +2,59 @@ package com.jordan.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.jordan.motionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val TAG = "MotionLayout"
+    private var preProgress: Float = 0F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                Log.d(TAG, "onTransitionStarted: ")
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                Log.d(TAG, "onTransitionChange: $progress")
+                if(preProgress < progress) {
+                    motionLayout?.progress = 1F
+                } else {
+                    motionLayout?.progress = 0F
+                }
+                preProgress = progress
+                Log.d(TAG, "onTransitionChange: $progress")
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                Log.d(TAG, "onTransitionCompleted: ")
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                Log.d(TAG, "onTransitionTrigger: $progress")
+            }
+        })
     }
 }
